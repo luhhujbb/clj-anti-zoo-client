d433411816a2d4f1c9fc5683c45d6ef38e9a7737
Jean-Baptiste Besselat <jean-baptiste.besselat@linkfluence.net>
2017-06-28 18:33:50 +0200

    [SKELETON] library function skeleton

--- project.clj
+++ project.clj
@@ -3,4 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                  ;;for proxy
+                 [clj-http "2.1.0"]
+                 [cheshire "5.7.0"]
+                 ;; logging stuff
+                 [org.clojure/tools.logging "0.3.1"]
+                 [org.slf4j/slf4j-api "1.7.19"]
+                 [org.slf4j/slf4j-log4j12 "1.7.19"]
+                 [org.slf4j/jcl-over-slf4j "1.7.19"]])
