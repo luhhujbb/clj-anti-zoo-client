c8666a1b7be9ed9ea0ebde17ba386e61141565db
Jean-Baptiste Besselat <jean-baptiste.besselat@linkfluence.net>
2017-06-28 18:09:42 +0200

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-anti-zoo-client
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_anti_zoo_client/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-anti-zoo-client.core-test
+  (:require [clojure.test :refer :all]
+            [clj-anti-zoo-client.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
